af7b1bcc63944446f455561f29ff56f5f3d2629e
John Trupiano <jtrupiano@gmail.com>
2012-05-14 15:16:38 -0500

    Tailgate32 header/footer/comment form.

---
+++ wp-content/themes/tailgate32/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Tailgate32
+ */
+?>
+
+	</div><!-- #main -->
+
+  <div class="divider"></div>
+  <div id="footer">&nbsp;</div>
+</body>
+</html>
