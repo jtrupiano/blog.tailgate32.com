3aae62ebbefb77bc282f1c825da0a0f001be8c75
John Trupiano <jtrupiano@gmail.com>
2012-05-16 17:23:04 -0500

    Update Other Links.  Increase spacing in lists on sidebar.

--- wp-content/themes/tailgate32/functions.php
+++ wp-content/themes/tailgate32/functions.php
@@ -4,7 +4,9 @@
     echo "  <h3>Other Links</h3>";
     echo "  <ul>";
     echo "    <li><a href=\"http://tailgate32.com\">Tailgate32.com</a></li>";
-    echo "    <li><a href=\"http://blog.tailgate32.com\">Blog Home</a></li>";
+    echo "    <li><a href=\"http://facebook.com/tailgate32\" target=\"_blank\">Tailgate32 on Facebook</a></li>";
+    echo "    <li><a href=\"http://twitter.com/tailgate_32\" target=\"_blank\">@tailgate_32 on Twitter</a></li>";
+    echo "    <li><a href=\"http://www.kickstarter.com/projects/jtrupiano/2025207769?token=7b4cdd40\" target=\"_blank\">Tailgate32 Kickstarter Campaign</a></li>";
     echo "  </ul>";
     echo "</aside>";
   }
