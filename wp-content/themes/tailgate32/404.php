94c933174e1d2e0de3cf60dd1dec3458ebfeed27
John Trupiano <jtrupiano@gmail.com>
2012-05-16 11:23:04 -0500

    Style 404 page.  Hack a fix for weird vertical spacing on author sidebar.

---
+++ wp-content/themes/tailgate32/404.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package Tailgate32
+ */
+
+get_header(); ?>
+
+	<div id="primary">
+		<div id="content" role="main">
+
+			<article id="post-0" class="post error404 not-found">
+				<header class="entry-header">
+					<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
+				</header>
+
+				<div class="entry-content">
+					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'twentyeleven' ); ?></p>
+
+					<?php get_search_form(); ?>
+        </div>
+
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+
+<br class="clear" />
+
+<?php get_footer(); ?>
--- wp-content/themes/tailgate32/sidebar-author.php
+++ wp-content/themes/tailgate32/sidebar-author.php
@@ -3,8 +3,10 @@
 ?>
     <div id="secondary" class="widget-area" role="complementary">
       <aside class="widget author">
-        <?php echo get_avatar($authordata->ID, 96); ?>
-        <p><?php echo $authordata->user_description; ?></p>
+        <div>
+          <?php echo get_avatar($authordata->ID, 96); ?>
+          <p><?php echo $authordata->user_description; ?></p>
+        </div>
       </aside>
       <?php other_sidebar_links(); ?>
     </div>
