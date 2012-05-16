9ba258065687f16dddff1ba0417bd1800ad04411
John Trupiano <jtrupiano@gmail.com>
2012-05-16 11:12:25 -0500

    Style "no posts by author" page.

--- wp-content/themes/tailgate32/author.php
+++ wp-content/themes/tailgate32/author.php
@@ -40,7 +40,7 @@ get_header(); ?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+						<h2 class="entry-title"><?php _e( "No Posts By $authordata->display_name", 'twentyeleven' ); ?></h2>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
