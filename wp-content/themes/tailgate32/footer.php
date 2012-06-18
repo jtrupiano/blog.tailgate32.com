0ff05b6f4929c90dfe0b49b845de22096656f27a
John Trupiano <jtrupiano@gmail.com>
2012-06-18 16:15:15 -0500

    Remove tshirt-viewer

--- wp-content/themes/tailgate32/footer.php
+++ wp-content/themes/tailgate32/footer.php
@@ -15,24 +15,6 @@
   <div id="footer">&nbsp;</div>
 
   <div id="hidden-content">
-    <div id="tshirt-viewer">
-      <h3>Donate $25 and Get a Tailgate32 Shirt in Your Team's Colors!</h3>
-      <div class="left">
-        <img id="tshirt-front" src="http://assets.tailgate32.com/images/tshirts/arizona-front.png" alt="Tailgate32 T-Shirt Front" width="483" height="414" />
-        <div class="team-selector">
-          <label for="tshirt-options">Choose Your Team</label>
-          <select id="tshirt-options">
-          </select>
-        </div>
-      </div>
-      <div class="right">
-        <img id="tshirt-back" src="http://assets.tailgate32.com/images/tshirts/arizona-back.png" alt="Tailgate32 T-Shirt Back" width="483" height="414" />
-        <div class="button-container">
-          <a href="http://www.kickstarter.com/projects/jtrupiano/tailgate32-the-ultimate-football-fan-roadtrip" target="_blank"><img src="http://assets.tailgate32.com/images/submit-icon-40.png" /><span>I Want This Shirt!</span></a>
-        </div>
-      </div>
-      <br class="clear" />
-    </div>    
     <div id="contact-us">
       <iframe style="display: none;" name="contact.us.iframe" src="https://docs.google.com/spreadsheet/viewform?formkey=dEY2MXNvVDRBWlN1SEE0RHRyQU42d3c6MQ"></iframe>
       <div class="headline">
