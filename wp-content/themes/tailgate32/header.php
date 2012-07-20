86d7212675c63c752001fce6f30174249d8f9bf5
John Trupiano <jtrupiano@gmail.com>
2012-07-20 13:48:38 -0500

    Replace WP comments with FB comments

--- wp-content/themes/tailgate32/header.php
+++ wp-content/themes/tailgate32/header.php
@@ -81,7 +81,16 @@
 </head>
 
 <body <?php body_class(); ?>>
-
+  <!-- Facebook SDK -->
+    <div id="fb-root"></div>
+    <script type="text/javascript">(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=139581442806599";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+  <!-- End Facebook SDK -->
   <div id="navigation">
     <div id="social-media-header">
       <a href="https://twitter.com/#!/tailgate_32" target="_blank" title="Follow @tailgate_32 on Twitter"><img src="http://assets.tailgate32.com/images/icon_twitter.png" alt="See @tailgate_32 on Twitter" /></a>
